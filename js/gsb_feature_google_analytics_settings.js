9d9a1ff19e48f16f484caec807ba0ac60a91a0e9
Greg Mercer <gmercer@stanford.edu>
2015-07-09 13:48:25 -0700

    WP-5272 Removing Business Insight tracking. Will now be handled in GTM.

--- js/gsb_feature_google_analytics_settings.js
+++ js/gsb_feature_google_analytics_settings.js
@@ -25,50 +25,6 @@
           });
         }                
 
-        // config event clicks for Idea Story
-
-        // Top Stories
-        $(".node-type-idea-story .top-stories-wrapper a").click(function() {
-          ga('send', 'event', 'Idea Story - Editor\'s Picks', 'click');
-        });
-
-        // Explore More Title
-        $(".node-type-idea-story .explore-more-wrapper .field-name-title a").click(function() {
-          ga('send', 'event', 'Idea Story - Explore More Title', 'click');
-        });
-
-        // Explore More Taxomony
-        $(".node-type-idea-story .explore-more-wrapper .field-name-field-key-taxonomy a").click(function() {
-          ga('send', 'event', 'Idea Story - Explore More Taxonomy', 'click');
-        });
-
-        // Related Content
-        $(".node-type-idea-story .field-name-field-related-faculty a").click(function() {
-          ga('send', 'event', 'Idea Story - Related Faculty', 'click');
-        });
-
-        $(".node-type-idea-story .field-name-field-related-idea-story a").click(function() {
-          ga('send', 'event', 'Idea Story - Related Idea Story', 'click');
-        });
-
-        $(".node-type-idea-story .field-collection-item-field-related-other-unlimited a").click(function() {
-          ga('send', 'event', 'Idea Story - Related Other', 'click');
-        });
-
-        $(".node-type-idea-story .field-name-field-link-unlimited a").click(function() {
-          ga('send', 'event', 'Idea Story - Related Link', 'click');
-        });
-
-        // Top Key Taxonomy Terms
-        $(".node-type-idea-story  .group_wrapper_header .field-name-field-key-taxonomy a").click(function() {
-          ga('send', 'event', 'Idea Story - Top Key Taxonomy', 'click');
-        });
-
-        // Bottom Taxonomy
-        $(".node-type-idea-story .group_wrapper_taxonomy a").click(function() {
-          ga('send', 'event', 'Idea Story - Bottom Key Taxonomy', 'click');
-        });
-
       }); // end document.ready 
     } // end attach
   }; // end Drupal.behaviors.gsb_feature_google_analytics_settings
